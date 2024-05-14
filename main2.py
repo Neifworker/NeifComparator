60431a1973997d98c3b45d813da72d0f7d7724d3
Neifworker <neifworker@gmail.com>
2024-05-14 04:21:21 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,13 @@
+#My first code on Python
+#14.05.2024
+#4:19am
+def Comparator():
+    x = int(input('First number?'))
+    y = int(input('Twice number?'))
+    if x > y:
+        print('First number more than twice number')
+    elif x < y:
+        print('First number less than twice number')
+    else:
+        print('The numbers are equal')
+Comparator()
